8f296fac8d9afef943c0c8f5be8ace3ca3ac67be
Ferenc Karsany <karsany@karsany.hu>
2015-04-21 00:41:04 +0200

    initial commit

---
+++ src/main/java/hu/karsany/grantor/App.java
@@ -0,0 +1,13 @@
+package hu.karsany.grantor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
